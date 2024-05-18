71746d6ad2270d37fd2cc95ab5d9bc2a4422bc46
Andres Fonseca <andresitofonseca13@gmail.com>
2024-05-17 19:13:16 -0500

    latest changes

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import animalesDomesticos.Gato;
+import animalesDomesticos.Loro;
 import animalesDomesticos.Perro;
 import animalesSalvajes.Cocodrilo;
 import animalesSalvajes.Leon;
@@ -16,7 +18,37 @@ public class Main {
         for(int i =0; i<=50; i++){
             System.out.print("-");
         }
+
         System.out.println("");
+
+        Gato gato = new Gato("Michi", 5, 3, 0.3, "Negro", "Felino", "Oscar", true, "123df", true);
+        System.out.println(gato.vacunar());
+        System.out.println(gato.entrenar());
+        System.out.println(gato.jugar());
+        System.out.println(gato.respiracion());
+        System.out.println(gato.alimentacion());
+        System.out.println(gato.desplazamiento());
+        System.out.println(gato.reproduccion());
+        for(int i =0; i<=50; i++){
+            System.out.print("-");
+        }
+
+        System.out.println("");
+
+        Loro loro = new Loro("Paco", 1, 2, 0.1, "Verde", "Ave", "Pedro", true, "123s2rddf", false);
+        System.out.println(loro.vacunar());
+        System.out.println(loro.entrenar());
+        System.out.println(loro.jugar());
+        System.out.println(loro.respiracion());
+        System.out.println(loro.alimentacion());
+        System.out.println(loro.desplazamiento());
+        System.out.println(loro.reproduccion());
+        for(int i =0; i<=50; i++){
+            System.out.print("-");
+        }
+
+        System.out.println("");
+
         Pato lucas = new Pato("Lucas", 2.5, 3, 0.3, "Blanco", "Ave", "Lagos", 3, true);
         System.out.println(lucas.cazar());
         System.out.println(lucas.huir());
